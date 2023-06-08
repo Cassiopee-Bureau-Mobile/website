4562af964dd411ac033c3a287534154530eb6a76
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-06-08 16:51:19 +0200

    Footer

---
+++ frontend/src/components/Layout/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <footer className='flex flex-col justify-center border-t items-center'>
+            <p className='m-3 mb-5'>Guillemet Samuel - Safon Clément</p>
+            <span className='w-screen h-2 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600'></span>
+        </footer>
+    );
+}
