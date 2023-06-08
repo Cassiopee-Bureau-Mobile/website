2931eee3159f35e291ea94440ce8eb40b6c5cbf7
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-06-08 16:52:16 +0200

    A Skeleton Loader

---
+++ frontend/src/components/Skeleton.tsx
@@ -0,0 +1,25 @@
+interface SkeletonProps {
+    width?: string;
+    height?: string;
+    className?: string;
+    id?: string;
+}
+
+export default function Skeleton(props: SkeletonProps): JSX.Element {
+    return (
+        <div
+            style={{
+                width: props.width,
+                height: props.height
+            }}
+            className={'mx-2 h-2.5 animate-pulse bg-gray-300 rounded-full ' + props.className}>
+            {props.id !== undefined ? (
+                <input
+                    hidden
+                    id={props.id}></input>
+            ) : (
+                <></>
+            )}
+        </div>
+    );
+}
