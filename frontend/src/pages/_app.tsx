575c9556c9b12d89541890d5c131a0c1088c85a7
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-06-08 16:17:53 +0200

    Add the correct config and state

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,19 +1,33 @@
-import '@/styles/globals.css';
+import '@/styles/global.scss';
 
 import React from 'react';
 import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
+
+type SideBarContextProps = [sideBarFolded: boolean, setSideBarFolded: React.Dispatch<React.SetStateAction<boolean>>];
+
+const SideBarContext = React.createContext<SideBarContextProps>([false, () => {}]);
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     const [queryClient] = React.useState(() => new QueryClient());
+    const [sideBarFolded, setSideBarFolded] = React.useState(false);
 
     return (
-        <QueryClientProvider client={queryClient}>
-            <Hydrate state={pageProps.dehydratedState}>
-                <Component {...pageProps} />
-            </Hydrate>
-            <ReactQueryDevtools />
-        </QueryClientProvider>
+        <SessionProvider session={pageProps.session}>
+            <QueryClientProvider client={queryClient}>
+                <Hydrate state={pageProps.dehydratedState}>
+                    <SideBarContext.Provider value={[sideBarFolded, setSideBarFolded]}>
+                        <Component {...pageProps} />
+                    </SideBarContext.Provider>
+                </Hydrate>
+                <ReactQueryDevtools />
+            </QueryClientProvider>
+        </SessionProvider>
     );
 }
+
+const useSideBarContext = () => React.useContext(SideBarContext);
+
+export { useSideBarContext };
